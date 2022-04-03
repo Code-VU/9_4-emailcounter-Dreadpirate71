135153f807b2119ccc6cda77025c33f2a228d1fc
James Remus <James.Remus@veteransunited.com>
2022-04-03 18:06:59 -0500

    moved loops outside

--- counter.py
+++ counter.py
@@ -3,25 +3,27 @@ def countEmail():
     sender = []
     countEmails = dict()
     name = input("Enter file:")
-    #if len(name) < 1 : name = "mbox-short.txt"
+    if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
     for line in handle:
         if line.startswith("From "):
             words = line.split()
             sender.append(words[1])
-            #print (sender)
-            for person in sender: 
-                countEmails[person] = countEmails.get(person, 0) + 1
-            maxSender = None
-            maxCount = None
+    print (sender)
+    for person in sender: 
+        countEmails[person] = countEmails.get(person, 0) + 1
+    maxSender = None
+    maxCount = None
     for person, count in countEmails.items():
         if maxCount is None or count > maxCount:
             maxSender = person
             maxCount = count
-        
+    #print(countEmails)  
+    #print (sender) 
 
-    #mostFrom = max(countEmails, key=countEmails.get)   
+    mostFrom = max(countEmails, key=countEmails.get)   
     print (maxSender, maxCount)
+    #print (mostFrom)
     #print (countEmails)
 ## if you want to test locally before you try to sync
 ## uncomment countEmail() and run > python counter.py
